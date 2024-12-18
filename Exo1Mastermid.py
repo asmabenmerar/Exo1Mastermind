5d1aeb8f41414cba0f27ab25795e1c660790629b
Asma Ben merar <asmabenmerar09.com>
2024-12-18 15:22:43 +0100

    Partie 2 : Génération de la combinaison secrète

--- Exo1Mastermid.py
+++ Exo1Mastermid.py
@@ -1,6 +1,17 @@
+import random
+
+
 # Etape 1 : Preparation
 # Constantes du jeu
 combin_length = 4
 nmbr_tentative = 10
 colors = ['R', 'G', 'B', 'Y', 'O', 'P']
+
+
+
+
+#Etape 2 :Génération de la Combinaison Secrète
+# Fonction pour générer la combinaison secrète
+def combinaison_secrete():
+    return random.choices(colors, k=combin_length)  # Utilisation de random.choices
 
